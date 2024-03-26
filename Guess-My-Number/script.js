3abde9075c7a8371070f9552f62aff7545232f5c
mahdi-samadi <mahdisamaadi@gmail.com>
2024-03-26 21:19:08 +0330

    add

--- Guess-My-Number/script.js
+++ Guess-My-Number/script.js
@@ -1,1 +1,10 @@
 'use strict';
+// console.log(
+//   (document.querySelector('.message').textContent = '😎 Correct number')
+// );
+// console.log(document.querySelector('.guess').value);
+// document.querySelector('.guess').value = 23;
+
+document.querySelector('.check').addEventListener('click', function () {
+  console.log(document.querySelector('.guess').value);
+});
