2646edbb9f3bb5e794f691ea4c0c0f8987f3fe36
mahdi-samadi <mahdisamaadi@gmail.com>
2024-03-28 01:14:01 +0330

    final project 2

--- modal window/script.js
+++ modal window/script.js
@@ -3,23 +3,28 @@
 const modal = document.querySelector('.modal');
 const overlay = document.querySelector('.overlay');
 const btnCloseModal = document.querySelector('.close-modal');
-console.log(btnCloseModal);
 const btnOpenModal = document.querySelectorAll('.show-modal');
-console.log(btnOpenModal);
 
 const openModal = function () {
   console.log('maaaaaaaaaaaahdi');
   modal.classList.remove('hidden');
   overlay.classList.remove('hidden');
 };
-
 const closeModal = function () {
   modal.classList.add('hidden');
   overlay.classList.add('hidden');
 };
+
 for (let i = 0; i < btnOpenModal.length; i++) {
   btnOpenModal[i].addEventListener('click', openModal);
 }
-
 btnCloseModal.addEventListener('click', closeModal);
 overlay.addEventListener('click', closeModal);
+
+document.addEventListener('keyup', function (b) {
+  if (b.key === 'Escape') {
+    if (!modal.classList.contains('hidden')) {
+      closeModal();
+    }
+  }
+});
