55ce2abd3c048a59dd325a9a781a599bfb24b18c
mahdi-samadi <mahdisamaadi@gmail.com>
2024-03-28 00:25:10 +0330

    final project

--- modal window/script.js
+++ modal window/script.js
@@ -2,13 +2,24 @@
 
 const modal = document.querySelector('.modal');
 const overlay = document.querySelector('.overlay');
-const closeModal = document.querySelector('.close-modal');
+const btnCloseModal = document.querySelector('.close-modal');
+console.log(btnCloseModal);
 const btnOpenModal = document.querySelectorAll('.show-modal');
 console.log(btnOpenModal);
+
+const openModal = function () {
+  console.log('maaaaaaaaaaaahdi');
+  modal.classList.remove('hidden');
+  overlay.classList.remove('hidden');
+};
+
+const closeModal = function () {
+  modal.classList.add('hidden');
+  overlay.classList.add('hidden');
+};
 for (let i = 0; i < btnOpenModal.length; i++) {
-  btnOpenModal[i].addEventListener('click', function () {
-    console.log('mahdi');
-    modal.classList.remove('hidden');
-    overlay.classList.remove('hidden');
-  });
+  btnOpenModal[i].addEventListener('click', openModal);
 }
+
+btnCloseModal.addEventListener('click', closeModal);
+overlay.addEventListener('click', closeModal);
