7d225175b42e50ce3d4e5748be2e9aabc87d3c33
mahdi-samadi <mahdisamaadi@gmail.com>
2024-03-27 23:50:05 +0330

    add code for script

--- modal window/script.js
+++ modal window/script.js
@@ -1,1 +1,14 @@
 'use strict';
+
+const modal = document.querySelector('.modal');
+const overlay = document.querySelector('.overlay');
+const closeModal = document.querySelector('.close-modal');
+const btnOpenModal = document.querySelectorAll('.show-modal');
+console.log(btnOpenModal);
+for (let i = 0; i < btnOpenModal.length; i++) {
+  btnOpenModal[i].addEventListener('click', function () {
+    console.log('mahdi');
+    modal.classList.remove('hidden');
+    overlay.classList.remove('hidden');
+  });
+}
