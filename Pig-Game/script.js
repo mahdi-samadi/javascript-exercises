61e69a81946ac4a06caa88baec2b4735cfbcb9b2
mahdi-samadi <mahdisamaadi@gmail.com>
2024-03-28 15:16:28 +0330

    add orginal files from dice project

---
+++ Pig-Game/script.js
@@ -0,0 +1,10 @@
+'use strict';
+// Selecting elements
+const score0E0 = document.querySelector('#score--0');
+const score1El = document.querySelector('#score--1');
+const diceEl = document.querySelector('.dice');
+
+// Starting conditions
+score0E0.textContent = 0;
+score1El.textContent = 0;
+diceEl.classList.add('hidden');
